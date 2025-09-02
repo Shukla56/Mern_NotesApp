c781ea7009f6352dd586ed17c3cdc43819acbcfb
Rahul shukla <115574091+Shukla56@users.noreply.github.com>
2025-09-02 23:17:01 +0530

    Update ApiEndPoint.js

--- Frontend/src/services/ApiEndPoint.js
+++ Frontend/src/services/ApiEndPoint.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://mern-notesapp-pqt4.onrender.com',
+    baseURL: 'https://mern-notesapp-pqt4.onrender.com,
     headers: {
         'Content-Type': 'application/json' // Removed extra space
     },
