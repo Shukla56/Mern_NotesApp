4680dc30a683858029f7523019096eada72e244b
Rahul shukla <115574091+Shukla56@users.noreply.github.com>
2025-09-05 16:21:52 +0530

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -11,8 +11,8 @@ export default function Login() {
   const dispatch=useDispatch()
   const navigate=useNavigate()
   const [value,setValue]=useState({
-    email:"  ",
-    password:"   "
+    email:"rahul@gmail.com",
+    password:"password"
   })
 
   const hanldeChange=(e)=>{
